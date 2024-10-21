06676d5206957f4861c335e1a4308b771a2acfba
Ethan <tylercemer@gmail.com>
2024-10-21 17:54:14 +0800

    vka logging in reusable verifier

--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,13 @@
 use crate::{
-    codegen::{AccumulatorEncoding, BatchOpenScheme::Bdfg21, SolidityGenerator},
+    codegen::{
+        util::to_u256_be_bytes, AccumulatorEncoding, BatchOpenScheme::Bdfg21, SolidityGenerator,
+    },
     encode_calldata,
     evm::test::{compile_solidity, Evm},
     FN_SIG_VERIFY_PROOF, FN_SIG_VERIFY_PROOF_WITH_VK_ADDRESS,
 };
 use halo2_proofs::halo2curves::bn256::{Bn256, Fr};
+use itertools::chain;
 use rand::Rng;
 use rand::{rngs::StdRng, RngCore, SeedableRng};
 use revm::primitives::Address;
@@ -111,14 +114,29 @@ fn run_render_separately<C: halo2::TestCircuit<Fr>>() {
         let vk_creation_code = compile_solidity(&vk_solidity);
         let vk_creation_code_size = vk_creation_code.len();
         println!("VK creation code size: {vk_creation_code_size}");
-        let (vk_address, gas_cost) = evm.create(vk_creation_code);
-        let vk_runtime_code_size = evm.code_size(vk_address);
+        // replace the creation call here with a evm.call to the deployVKA command on the
+        // reusable verifier
+
+        let encoded_calldata = chain![
+            [0xde, 0x8f, 0x56, 0x58], // function signature "deployVKA(bytes)"
+            to_u256_be_bytes(0x20),   // offset of vka bytecode
+            to_u256_be_bytes(vk_creation_code_size), // length of vka bytecode
+            vk_creation_code.iter().cloned()
+        ]
+        .collect();
+        let (_gas_cost, output) = evm.call(verifier_address, encoded_calldata);
+
+        let word: [u8; 32] = output.try_into().unwrap();
+        let vk_address: [u8; 20] = word[12..32].try_into().unwrap();
+
+        // let (vk_address, gas_cost) = evm.create(vk_creation_code);
+        let vk_runtime_code_size = evm.code_size(vk_address.into());
         println!("VK runtime code size: {vk_runtime_code_size}");
-        println!("Gas deployment cost VK: {gas_cost}");
+        // println!("Gas deployment cost VK: {gas_cost}");
 
         let (gas_cost, output) = evm.call(
             verifier_address,
-            encode_calldata(Some(vk_address.into()), &proof, &instances),
+            encode_calldata(Some(vk_address), &proof, &instances),
         );
         assert_eq!(output, [vec![0; 31], vec![1]].concat());
         println!("Gas cost separate: {gas_cost}");
